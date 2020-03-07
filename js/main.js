56ef412eda140f4c9fda79c001d9991884b4517d
thasnisalim <61856851+thasnisalim@users.noreply.github.com>
2020-03-07 08:18:16 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Thasni salim", "I'm a jaurnalist", "Love Simplicity"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
